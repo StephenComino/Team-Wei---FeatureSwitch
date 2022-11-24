025d6d58fa664c1f86f3141b1b2dfbb7439c51d4
Wei Zhou <wei.zhou@aristocrat.com>
2022-11-24 20:51:20 +1100

    Added the workshop solution

---
+++ FeatureSwitch/FeatureSwitch.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeatureSwitch
+{
+    public class FeatureSwitch : IFeatureSwitch
+    {
+        public void Enabled(string feature, string version)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FeatureSwitchApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace FeatureSwitch
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = Host.CreateDefaultBuilder(args).ConfigureServices((_, services) =>
+            {
+                services.AddScoped<IFeatureSwitch, FeatureSwitch>();
+            }).Build();
+
+            host.Run();
+
+        }
+    }
+}
