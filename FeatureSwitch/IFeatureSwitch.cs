2eeb46b3d0fbda98629866475cf9504f3b374518
Henry Li <henry.li@aristocrat.com>
2022-11-25 13:24:22 +1100

    Update IFeatureSwitch.cs
    Add a temporary class to remove compile error

--- FeatureSwitch/IFeatureSwitch.cs
+++ FeatureSwitch/IFeatureSwitch.cs
@@ -10,4 +10,36 @@ namespace FeatureSwitch
         // Interface for load module to get app path
         public string GetPath();
     }
+    
+    //DO NOT CHANGE
+    public class FeatureConfigEntity
+    {
+        public string App { get; set; }
+        public string UserGroup { get; set; }
+        public string IpMask { get; set; }
+        public string Device { get; set; }
+
+        public string[] CustomFields { get; set; } = new string[5];
+
+        public string FeatureCode { get; set; }
+
+        public string Version { get; set; }
+
+        public bool Enabled { get; set; }
+
+
+
+        // skeleton functions only
+        public bool IsIpSelected(string ip)
+        {
+            // fake logic
+            return true;
+        }
+        public bool IsUserSelected(string userid)
+        {
+            // fake logic
+            return true;
+        }
+
+    }
 }
