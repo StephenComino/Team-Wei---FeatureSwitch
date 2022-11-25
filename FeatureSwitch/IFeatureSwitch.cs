854ce8deaf1eda71641f82457bac3dfe80ad5e2e
Henry Li <henry.li@aristocrat.com>
2022-11-25 16:32:11 +1100

    Update IFeatureSwitch.cs
    Use IConfigurationProvider instead of ConfigurationManager

--- FeatureSwitch/IFeatureSwitch.cs
+++ FeatureSwitch/IFeatureSwitch.cs
@@ -1,7 +1,5 @@
+using System.Collections.Generic;
 using ConfigurationProvider.Models;
-using FeatureSwitch.Contract;
-
-
 
 namespace FeatureSwitch
 {
@@ -10,12 +8,16 @@ namespace FeatureSwitch
         // register features for target app ? path
         public void RegisterFeature(FeatureConfiguration config, string target);
 
-        public void RegisterFeature(FeatureConfiguration config, IFeatureApp instance);
+        public void RegisterFeature(FeatureConfiguration config, ITickManager instance);
 
         // Interface for load module to get app path
         public string GetPath();
 
-        public IFeatureApp Get(FeatureConfiguration config);
+        public ITickManager GetInstance();
     }
 
+    public interface ITickManager
+    {
+
+    }
 }
