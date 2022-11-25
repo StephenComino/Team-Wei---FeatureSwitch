d71d724418804b65b3ec37868f8f0824af08f53e
Henry Li <henry.li@aristocrat.com>
2022-11-25 12:59:23 +1100

    Add files via upload
    Add feature.cs

---
+++ FeatureSwitch/Feature.cs
@@ -0,0 +1,21 @@
+﻿namespace FeatureSwitch
+{
+    public struct Feature
+    {
+        public FeatureNames Name;
+        public string Value;
+        public string Version;
+        public bool Enabled;
+        public bool Optional;
+    }
+
+    public enum FeatureNames
+    {
+        AppCode,
+        UserGroup,
+        IpMask,
+        Device,
+        FeatureCode,
+        CustomField,
+    }
+}
