70e12ce90990a1e22b57125b1611880afa621bf4
Henry Li <henry.li@aristocrat.com>
2022-11-25 13:00:13 +1100

    Update FeatureSwitch.cs
    First draft for feature switch logic

--- FeatureSwitch/FeatureSwitch.cs
+++ FeatureSwitch/FeatureSwitch.cs
@@ -1,16 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Workshop2022.API.Models.Services;
 
 namespace FeatureSwitch
 {
     public class FeatureSwitch : IFeatureSwitch
     {
-        public void Enabled(string feature, string version)
+        private readonly Dictionary<string, List<Feature>> _registerFeatures = new Dictionary<string, List<Feature>>();
+        private List<Feature> _features;
+        private string _defaultPath = "";
+
+        // Register features for app
+        public void RegisterFeature(FeatureConfigEntity config, string target)
         {
-            throw new NotImplementedException();
+            var features = new List<Feature>();
+            if (config == null || config.App == null || config.App == "")
+                throw new Exception("Null config or app");
+            
+            // todo: check if every feature is enabled
+            features.Add(new Feature { Name=FeatureNames.AppCode, Value=config.App, Version=config.Version, Enabled=config.Enabled, Optional=false});
+            features.Add(new Feature { Name = FeatureNames.UserGroup, Value = config.UserGroup, Version = "", Enabled = config.Enabled, Optional = true });
+            features.Add(new Feature { Name = FeatureNames.IpMask, Value = config.IpMask, Version = "", Enabled = config.Enabled, Optional = true });
+            features.Add(new Feature { Name = FeatureNames.Device, Value = config.Device, Version = "", Enabled = config.Enabled, Optional = true });
+
+            foreach (var l in config.CustomFields)
+            {
+                features.Add(new Feature { Name = FeatureNames.CustomField, Value = l, Version = "", Enabled = config.Enabled, Optional = true });
+            }
+
+            _registerFeatures.Add(target, features);
+        }
+
+        // match features to a target app path
+        public string GetPath()
+        {
+            // use features from GetFeaturesFromConfig() to match the features and return the path
+            foreach (var key in _registerFeatures.Keys)
+            {
+                // multiple matches?
+                if (_registerFeatures[key].Except(_features).ToList().Count == 0)
+                    return key;
+            }
+            // todo: return app type whether it is dll, web service or winform
+            return _defaultPath;
+        }
+
+        // call Interface from storage to get feature set
+        public void GetFeaturesFromConfig()
+        {
+            // set features
         }
     }
+
+    
 }
