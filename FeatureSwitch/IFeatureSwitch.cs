df6daabab572b26b50b8bbd42a53040b90ad7373
Henry Li <henry.li@aristocrat.com>
2022-11-25 13:20:14 +1100

    Update IFeatureSwitch.cs
    change input para

--- FeatureSwitch/IFeatureSwitch.cs
+++ FeatureSwitch/IFeatureSwitch.cs
@@ -5,7 +5,7 @@ namespace FeatureSwitch
     public interface IFeatureSwitch
     {
         // register features for target app ? path
-        public void RegisterFeature(List<Feature> features, string target);
+        public void RegisterFeature(FeatureConfigEntity config, string target);
 
         // Interface for load module to get app path
         public string GetPath();
